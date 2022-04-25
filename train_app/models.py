f66a93cee97a14f4d250fcceffa11e4f1367028f
ifalice <mr.spankibsater@gmail.com>
2022-04-25 15:28:14 +0300

    create UserModels

--- train_app/models.py
+++ train_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class UserModels(models.Model):
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40, blank=True)
+    city = models.CharField(max_length=40, blank=True)
+    email = models.EmailField()
+    password1 = models.CharField(max_length = 40)
+    password2 = models.CharField(max_length = 40)
+    super_user = models.BooleanField()
+    user_auth = models.BooleanField()
+
+
+    
